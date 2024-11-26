1fa42eb60d0684589357278d672fa7423b725263
VarunSankuri <sankurivarun@gmail.com>
2024-11-25 18:08:33 -0600

    Update apprag2.py

--- apprag2.py
+++ apprag2.py
@@ -1,4 +1,4 @@
-theimport streamlit as st
+import streamlit as st
 from langchain.prompts import PromptTemplate
 from langchain.chains.question_answering import load_qa_chain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
