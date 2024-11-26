5855a8b12c7438ef008ff852f1dfa4d87ca2778b
VarunSankuri <sankurivarun@gmail.com>
2024-11-25 21:55:32 -0600

    Update apprag2.py

--- apprag2.py
+++ apprag2.py
@@ -30,7 +30,8 @@ st.markdown("""
 st.markdown('<p class="big-font">Developed by Varun Sankuri</p>', unsafe_allow_html=True)
 st.caption("Frustrated with ChatGPT and Google Gemini giving you outdated cloud info? Big box models can't keep up with the Cloud's rapid pace."
            " CloudCurrent is updated much more frequently and also lets you upload your OWN PDFs to get the most accurate, "
-           "up-to-the-minute answers. Try it now! /n For questions contact cloudcurrent@gmail.com")
+           "up-to-the-minute answers. Try it now!)
+st.cation("For questions contact cloudcurrent@gmail.com")
 
 # Load environment variables from .env file
 load_dotenv()
