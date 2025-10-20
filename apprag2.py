e145b7693047ca0e20cc1b1708c2ef93a741471f
VarunSankuri <sankurivarun@gmail.com>
2025-10-20 11:16:31 -0500

    Refactor import statements and clean up code

--- apprag2.py
+++ apprag2.py
@@ -1,9 +1,8 @@
-# --- START: Replace all your imports with this block ---
+# --- START: Use this new import block ---
 import streamlit as st
 from langchain_core.prompts import PromptTemplate
-from langchain_classic.chains import load_qa_chain
+from langchain_classic.chains.question_answering import load_qa_chain
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-# from langchain.vectorstores import Chroma
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 import PyPDF2
@@ -12,22 +11,18 @@ import io
 from langchain_community.vectorstores import Chroma
 import pysqlite3  # Add this import
 import sys      # Add this import
-from langchain.agents import AgentType, initialize_agent
-from langchain_community.agent_toolkits.load_tools import load_tools
-from langchain_community.utilities import GoogleSearchAPIWrapper
 import pandas as pd
 import re
 import plotly.express as px
 from graphviz import Source
 import graphviz
 from google.api_core import client_options
-from langchain.memory import ConversationBufferMemory
-from langchain_community.tools import DuckDuckGoSearchRun
+
 # Swap sqlite3 with pysqlite3-binary
 sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 import chromadb
 chromadb.api.client.SharedSystemClient.clear_system_cache()
-# --- END: Your import block is now correct ---
+# --- END: Your import block is now clean ---
 
 # --- SIDEBAR ---
 # --- SIDEBAR CONFIGURATION ---
